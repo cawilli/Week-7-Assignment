a76c5dc032f5bb661792bb1e61ddcfef740b0cd5
Candace Williams <candace.williams94@hotmail.com>
2021-08-28 12:14:39 -0400

    override toString with stringbuilder to allow for school info output.

--- School.cs
+++ School.cs
@@ -36,33 +36,45 @@ namespace Week_7_Assignment
 
                 }
             }
-            //your constructor usually handles intialization.
-            //your constructor needs to be the same name as your namespace
-            //can also get a constructor by typing ctor and hit tab x2
-            public School()
-                {
-                Name = "Untitled School";
-                PhoneNumber = "555-1234";
-                }
-            public School(string SchoolName, string SchoolPhoneNumber)
-                {
-                Name = SchoolName;
-                PhoneNumber = SchoolPhoneNumber;
-                }
-            //added a method. Added both float and int but provides the same result
-            //the two method were overloaded. Commented out one method. 
-            //public float AverageThreeScores(float a, float b, float c)
-            //    {
-            //    var result = (a + b + c) / 3;
-            //    return result;
-            //    }
-            public int AverageThreeScores(int a, int b, int c)
-                {
-                var result = (a + b + c) / 3;
-                return result;
-                }
-            //function bodied expressions are a more concise way to write a method. 
-            //function bodied expressions are for simple expressions. Don't do it for long code 
-            public float AverageThreeScores(float a, float b, float c) => (a + b + c) / 3;
+         //your constructor usually handles intialization.
+         //your constructor needs to be the same name as your namespace
+         //can also get a constructor by typing ctor and hit tab x2
+         public School()
+             {
+                 Name = "Untitled School";
+                 PhoneNumber = "555-1234";
+             }
+         public School(string SchoolName, string SchoolPhoneNumber)
+             {
+                 Name = SchoolName;
+                 PhoneNumber = SchoolPhoneNumber;
+             }
+         //added a method. Added both float and int but provides the same result
+         //the two method were overloaded. Commented out one method. 
+         //public float AverageThreeScores(float a, float b, float c)
+         //    {
+         //    var result = (a + b + c) / 3;
+         //    return result;
+         //    }
+          public static int AverageThreeScores(int a, int b, int c)
+             {
+                 var result = (a + b + c) / 3;
+                 return result;
+             }
+          //function bodied expressions are a more concise way to write a method. 
+          //function bodied expressions are for simple expressions. Don't do it for long code 
+          public static float AverageThreeScores(float a, float b, float c) => (a + b + c) / 3;
+          public override string ToString()
+             {
+                var sb = new StringBuilder();
+                sb.AppendLine(this.Name);
+                sb.AppendLine(this.Address);
+                sb.AppendLine(this.City);
+                sb.Append(", ");
+                sb.Append(this.State);
+                sb.Append(" ").
+                sb.Append(this.Zip);
+                return sb.ToString();
+             }
     }
 }
