8c7117d1ee4fdf15bf332bfe693c54c379ff48f6
Candace Williams <candace.williams94@hotmail.com>
2021-08-28 12:39:48 -0400

    add forms app.

---
+++ SchoolFormsApp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week_7_Assignment
+{
+    public partial class SchoolFormsApp : Form
+    {
+        public SchoolFormsApp()
+        {
+            InitializeComponent();
+        }
+    }
+}
