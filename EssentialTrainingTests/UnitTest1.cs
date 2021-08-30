e431c15203a37fb8176b17aa77e52d8ee71ed8fd
Candace Williams <candace.williams94@hotmail.com>
2021-08-30 19:44:00 -0400

    fix folder issues

---
+++ EssentialTrainingTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Week_7_Assignment;
+
+namespace EssentialTrainingTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var testInstance = new Program();
+            var testResult = testInstance.AddTwo(9, 5);
+            Assert.AreEqual(14, testResult, "I expect 9 + 5 to be 14");
+        }
+    }
+}
