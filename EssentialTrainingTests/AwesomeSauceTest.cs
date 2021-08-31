d9d69f82e5147ede2567c2d79fb955e512fcf9cf
Candace Williams <candace.williams94@hotmail.com>
2021-08-30 23:07:49 -0400

    build basic unit test to test lists and successfully run tests with no errors.

---
+++ EssentialTrainingTests/AwesomeSauceTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Week_7_Assignment;
+
+namespace EssentialTrainingTests
+{
+    [TestClass]
+    public class AwesomeSauceTest
+    {
+        [TestMethod]
+        public void IsSauceAwesomeTest()
+        {
+            var testInstance = new AwesomeSauce();
+            
+            testInstance.Sauces.Add("Chalula");
+            testInstance.Sauces.Add("Sweet Chili");
+            testInstance.Sauces.Add("Trailer Trash");
+
+            //expect true 
+            Assert.IsTrue(testInstance.IsSauceAwesome("Sweet Chili"));
+
+            //expect false 
+            Assert.IsFalse(testInstance.IsSauceAwesome("Red Hot"));
+
+        }
+    }
+}
