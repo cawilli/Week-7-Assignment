18adb91ad091fb53addd89082e671af799fa360f
Candace Williams <candace.williams94@hotmail.com>
2021-08-30 22:43:01 -0400

    add new class to work with an instantiate list.

--- AwesomeSauce.cs
+++ AwesomeSauce.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace Week_7_Assignment
 {
-    class AwesomeSauce
+   public class AwesomeSauce
     {
+        public List<string> Sauces { get; set; }
+
+        public AwesomeSauce()
+        {
+            Sauces = new List<string>();
+        }
+        public bool IsSauceAwesome(string sauce)
+        {
+            return Sauces.Contains(sauce);
+        }
     }
 }
