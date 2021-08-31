c125eedd1a85b88ec43d5a7cbb1b231febc05484
Candace Williams <candace.williams94@hotmail.com>
2021-08-31 09:13:07 -0400

    add foreach loop.

--- Programming Loops.cs
+++ Programming Loops.cs
@@ -8,6 +8,16 @@ namespace Week_7_Assignment
 {
     public class Programming_Loops
     {
+        public int ForEachLoop()
+        {
+            var numbers = new List<int> { 1, 3, 5, 7, 9 };
+            var sum = 0;
+            foreach(var number in numbers)
+            {
+                sum += number;
+            }
+            return sum;
+        }
         public void ForLoop()
         {
             var sum = 0;
