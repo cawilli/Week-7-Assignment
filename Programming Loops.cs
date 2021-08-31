c4f388737f2d61f15b3d32c4ac999084e3c3b999
Candace Williams <candace.williams94@hotmail.com>
2021-08-31 09:05:46 -0400

    add forloop.

---
+++ Programming Loops.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_7_Assignment
+{
+    class Programming_Loops
+    {
+        public void ForLoop()
+        {
+            var sum = 0;
+            for (var i = 0; i < 100; i++)
+            {
+                sum = sum + i;
+            }
+
+        }
+    }
+}
