f98cab4d84769cae3805a6e92d741adf66baf247
Candace Williams <candace.williams94@hotmail.com>
2021-08-31 09:19:56 -0400

    add while loop and dowhile loop.

--- Programming Loops.cs
+++ Programming Loops.cs
@@ -8,6 +8,30 @@ namespace Week_7_Assignment
 {
     public class Programming_Loops
     {
+        public int DoWhileLoop()
+        {
+            var sum = 0;
+            var counter = 0;
+            do
+            {
+                sum += counter;
+                counter++;
+            }
+            while (counter < 100);
+
+            return sum;
+        }
+        public int WhileLoop()
+        {
+            var sum = 0;
+            var counter = 0;
+            while(counter < 100)
+            {
+                sum += counter;
+                counter++;
+            }
+            return sum;
+        }
         public int ForEachLoop()
         {
             var numbers = new List<int> { 1, 3, 5, 7, 9 };
